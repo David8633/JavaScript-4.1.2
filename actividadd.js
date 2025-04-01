13f78038a5950a38da0a396b93ba01e68778c263
David8633 <davidinstituto282@gmail.com>
2025-04-01 10:10:25 +0200

    Subida de actividad 2 de ejercicio JsString

--- actividadd.js
+++ actividadd.js
@@ -1,2 +1,4 @@
 /*Declara una variable cuyo nombre sea cadena y tenga el valor “Hola Mundo”.-**/
 let nombre = "Hola mundo";
+/*Muestra por consola la longitud de la cadena.*/
+console.log(nombre.length);
